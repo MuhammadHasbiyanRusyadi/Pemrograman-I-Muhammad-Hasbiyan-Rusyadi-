4ee8a2750da6b03b440b994ff0215437c18abaf3
Hasbi <145199822+MuhammadHasbiyanRusyadi@users.noreply.github.com>
2023-12-18 23:44:38 +0800

    Delete Python directory

--- Python/PRAK002-2310817210020-Muhammad_Hasbiyan_Rusyadi.py
+++
@@ -1,3 +0,0 @@
-print("Selamat Pagi, Muhammad Hasbiyan Rusyadi")
-print("Selamat Siang, Muhammad Hasbiyan Rusyadi")
-print("Selamat Malam, Muhammad Hasbiyan Rusyadi")
--- Python/PRAK004-2310817210020-Muhammad_Hasbiyan_Rusyadi.py
+++
@@ -1,8 +0,0 @@
-nama = "Muhammad Hasbiyan Rusyadi"
-nim = "2310817210020"
-print("##############################")
-print("#                            #")
-print(f"#  {nama} #")
-print(f"#       {nim}        #")
-print("#                            #")
-print("##############################")
