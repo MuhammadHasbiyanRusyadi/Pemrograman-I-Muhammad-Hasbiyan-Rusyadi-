f32d876b1c26184249aee01800127d8fb5581b91
Hasbi <145199822+MuhammadHasbiyanRusyadi@users.noreply.github.com>
2023-12-18 23:44:27 +0800

    Delete C directory

--- C/PRAK002-2310817210020-Muhammad_Hasbiyan_Rusyadi.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main()  {
-    printf("Selamat Pagi, Muhammad Hasbiyan Rusyadi\n");
-    printf("Selamat Siang, Muhammmad Hasbiyan Rusyadi\n");
-    printf("Selamat Malam, Muhammad Hasbiyan Rusyadi\n");
-}
--- C/PRAK003-2310817210020-Muhammad_Hasbiyan_Rusyadi.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main()  {
-  char kalimat[25] = "Saya Pasti Bisa";
-  printf("Andi Berkata \"%s\" ", kalimat);
-  return 0;  }
